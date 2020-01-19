7b4d847a2d73082130321d1ea9b74e079fdfbc3e
Teodor Heggelund <git+t480s@teodorheggelund.com>
2020-01-19 08:57:51 +0100

    Cleanup install script

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-mkdir -p ~/.local/bin
-mkdir -p ~/.local/share/iterbart-rofi
-curl -o ~/.local/bin/iterbart https://raw.githubusercontent.com/iterate/iterbart-rofi/master/iterbart_rofi.py
-curl -o ~/.local/share/iterbart-rofi/links.json https://iterbart.app.iterate.no/data/links.json
-chmod +x ~/.local/bin/iterbart
+BINFOLDER="~/.local/bin"
+DATAFOLDER="~/.local/share/iterbart-rofi"
+
+ITERBART_BINARY="${BINFOLDER}/iterbart"
+
+SCRIPT_URL="https://raw.githubusercontent.com/iterate/iterbart-rofi/master/iterbart_rofi.py"
+JSON_URL="https://iterbart.app.iterate.no/data/links.json"
+
+echo "Creating folders `$BINFOLDER` and `$DATAFOLDER` ..."
+mkdir -p "$BINFOLDER" "$DATAFOLDER"
+
+echo "Downloading resources ..."
+curl -s -o "$ITERBART_BINARY" "$SCRIPT_URL"
+curl -s -o "${DATAFOLDER}/links.json" "$JSON_URL"
+
+echo "Making `${ITERBART_BINARY}` executable ..."
+chmod +x "$ITERBART_BINARY"
+
+echo "Iterbart installed."
